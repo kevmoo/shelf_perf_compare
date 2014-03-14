683a1d0429834e704dd82469ee777a563d53a8f0
Kevin Moore <github@j832.com>
2014-03-14 09:53:44 -0700

    tiny tweak

--- bin/dart_io.dart
+++ bin/dart_io.dart
@@ -8,7 +8,7 @@ void main() {
 
 void serve([int port = 8080]) {
   HttpServer.bind('127.0.0.1', port).then((server){
-    print('Serving dart:io simple @ ${server.address.host}:${server.port}');
+    print('dart:io simple @ ${server.address.host}:${server.port}');
     server.listen((HttpRequest request) {
       request.response.write("Hello, world");
       request.response.close();
